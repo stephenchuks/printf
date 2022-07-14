03b1b57d08bf51d2814f76d87904bc11871ae371
InyangUkpong <inyangweb@gmail.com>
2022-07-14 03:52:31 -0700

    format edits to _printf.c

--- print_c.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-#include <stdarg.h>
-
-int print_c(va_list args)
-
-{
-    char c = va_arg(args, int);
-    return (_putchar(c));
-}
--- print_i.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-#include <stdarg.h>
-
-int print_i(va_list args)
-{
-    int n = va_arg(args, int);
-    
-    return (n);
-} 
